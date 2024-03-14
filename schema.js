00b7bbd4e40523c5dbc658304a34b701ee682619
Nitin Pund <nitinpund@ymail.com>
2024-03-14 12:04:40 +0530

    related data examples

--- schema.js
+++ schema.js
@@ -2,17 +2,21 @@ export const typeDefs = `#graphql
     type Game {
         id:ID!
         title:String!
-        platform: [String]!
+        platform: [String!]!
+        reviews:[Review!]
     }
     type Review {
         id:ID!
         rating:Int!
         content:String!
+        game:Game!
+        author:Author!
     }
     type Author {
         id:ID!
         name:String!
         verified:Boolean!
+        reviews:[Review!]
     }
     type Query {
         reviews:[Review]
